eb2f0f3daf66f347d6971cd22575334f06b6eca7
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2024-05-15 22:38:20 -0600

    Se agregan nuevos componentes de tremor

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,77 +1,96 @@
-import { useState } from 'react';
-import { Card, Metric, Text, Title, DonutChart, LineChart, DatePicker  } from '@tremor/react';
-import { es } from 'date-fns/locale';
-import { CategoryBar } from '@tremor/react';
+import { Card, Metric, Text, Title, DonutChart, LineChart, DatePicker, CategoryBar, Legend } from '@tremor/react';
 import './Dashboard.css';
 import dataBarbie from '../../movie-barbie.json';
 import dataOppenheimer from '../../movie-oppenheimer.json';
 
-
 const Dashboard = () => {
+  const chartData = dataBarbie.domestic_daily.map(({ revenue, date }) => {
+    const oppenheimer = dataOppenheimer.domestic_daily.find(opp => opp.date === date);
+    return {
+      date,
+      Barbie: revenue,
+      Oppenheimer: oppenheimer?.revenue,
+    };
+  });
 
-const chartData = dataBarbie.domestic_daily.map(({ revenue, date }) => {
-  const oppenheimer = dataOppenheimer.domestic_daily.find(opp => opp.date === date);
-  return {
-    date,
-    Barbie: revenue,
-    Oppenheimer: oppenheimer?.revenue,
+  const addCommasToNumber = (number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
-});
 
-function addCommasToNumber(number) {
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+  const valor = 3000;
+  const valor2 = 42;
 
-const valor = 3000;
-const valor2 = 42;
-
-
+  const sales = [
+    {
+      name: 'New York',
+      sales: 980,
+    },
+    {
+      name: 'London',
+      sales: 456,
+    },
+    {
+      name: 'Hong Kong',
+      sales: 390,
+    },
+    {
+      name: 'San Francisco',
+      sales: 240,
+    },
+    {
+      name: 'Singapore',
+      sales: 190,
+    },
+  ];
+  
+  const valueFormatter = (number) =>
+    `$ ${Intl.NumberFormat('us').format(number).toString()}`;
 
   return (
     <div className="text-left">
       <div className="grid grid-cols-2 gap-12">
+        {/* Plagas */}
         <div>
-          <h2 className="text-2xl font-bold mb-6">Barbie</h2>
-          
+          <h2 className="text-2xl font-bold mb-6">Plagas</h2>
 
-
-
-          <Card className="max-w-lg mb-6 h-30">
+          {/* Componente de Dona */}
+          <Card className="max-w-lg mb-6 h-30 text-tremor-content">
             <Title>Sales</Title>
-            <DonutChart
-              className="mt-6 mb-6 h-40"
-              data={[
-                {
-                  name: 'false',
-                  userScore: dataBarbie.vote_average,
-                },
-                {
-                  name: 'false',
-                  userScore: 10 - dataBarbie.vote_average,
-                },
-              ]}
-              category="userScore"
-              index="name"
-              colors={['blue', 'slate']}
-              label={`${(dataBarbie.vote_average * 10).toFixed()}%`}
-            />
+            <div className="flex items-center justify-center space-x-6">
+              <DonutChart
+                data={sales}
+                category="sales"
+                index="name"
+                valueFormatter={valueFormatter}
+                colors={["blue", "cyan", "indigo", "violet", "fuchsia"]}
+                className="w-40"
+              />
+              <Legend
+                categories={[
+                  "New York",
+                  "London",
+                  "Hong Kong",
+                  "San Francisco",
+                  "Singapore",
+                ]}
+                colors={["blue", "cyan", "indigo", "violet", "fuchsia"]}
+                className="max-w-xs"
+              />
+            </div>
           </Card>
 
-            <p className="text-center font-mono text-sm text-slate-500">
-              Date Picker
-            </p>
-            <DatePicker />
-
-          <Card className="max-w-xs mx-auto mb-6" decoration="top" decorationColor="indigo">
+          {/* Componentes para mostrar un valor numérico de manera destacada (Metric) */}
+          <Card className="max-w-xs mx-auto mb-6 decoration-top decorationColor-indigo">
             <Text>Revenue</Text>
             <Metric>${addCommasToNumber(dataBarbie.global_revenue)}</Metric>
           </Card>
 
-          <Card className="max-w-xs mx-auto mb-6" decoration="top" decorationColor="indigo">
+          <Card className="max-w-xs mx-auto mb-6 decoration-top decorationColor-indigo">
             <Text>Budget</Text>
             <Metric>${valor}</Metric>
           </Card>
 
+          {/* Componente CategoryBar (INDICES DE 0 A 100) */}
           <Card className="mx-auto max-w-sm">
             <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content flex items-center justify-between">
               <span>Rating Product A</span>
@@ -79,59 +98,91 @@ const valor2 = 42;
             </p>
             <CategoryBar
               values={[40, 30, 20, 10]}
-              colors={['emerald', 'yellow', 'orange', 'rose']}
+              colors={["emerald", "yellow", "orange", "rose"]}
               markerValue={valor2}
               className="mt-3"
             />
           </Card>
         </div>
 
+        {/* Enfermedades */}
         <div>
-          <h2 className="text-2xl font-bold mb-6">Oppenheimer</h2>
-          <Card className="max-w-lg mb-6">
+          <h2 className="text-2xl font-bold mb-6">Enfermedades</h2>
+
+          {/* Componente de Dona */}
+          <Card className="max-w-lg mb-6 h-30 text-tremor-content">
             <Title>Sales</Title>
-            <DonutChart
-              className="mt-6 mb-6"
-              data={[
-                {
-                  name: 'false',
-                  userScore: dataOppenheimer.vote_average,
-                },
-                {
-                  name: 'false',
-                  userScore: 10 - dataOppenheimer.vote_average,
-                },
-              ]}
-              category="userScore"
-              index="name"
-              colors={['green', 'slate']}
-              label={`${(dataOppenheimer.vote_average * 10).toFixed()}%`}
-            />
+            <div className="flex items-center justify-center space-x-6">
+              <DonutChart
+                data={sales}
+                category="sales"
+                index="name"
+                valueFormatter={valueFormatter}
+                colors={["blue", "cyan", "indigo", "violet", "fuchsia"]}
+                className="w-40"
+              />
+              <Legend
+                categories={[
+                  "New York",
+                  "London",
+                  "Hong Kong",
+                  "San Francisco",
+                  "Singapore",
+                ]}
+                colors={["blue", "cyan", "indigo", "violet", "fuchsia"]}
+                className="max-w-xs"
+              />
+            </div>
           </Card>
-          <Card className="max-w-xs mx-auto mb-6" decoration="top" decorationColor="indigo">
+
+          {/* Componente para mostrar un valor numérico de manera destacada (Metric) */}
+          <Card className="max-w-xs mx-auto mb-6 decoration-top decorationColor-indigo">
             <Text>Revenue</Text>
-            <Metric>${addCommasToNumber(dataOppenheimer.global_revenue)}</Metric>
+            <Metric>
+              ${addCommasToNumber(dataOppenheimer.global_revenue)}
+            </Metric>
           </Card>
-          <Card className="max-w-xs mx-auto mb-6" decoration="top" decorationColor="indigo">
+          <Card className="max-w-xs mx-auto mb-6 decoration-top decorationColor-indigo">
             <Text>Budget</Text>
             <Metric>${addCommasToNumber(dataOppenheimer.budget)}</Metric>
           </Card>
+
+          {/* Componente CategoryBar (INDICES DE 0 A 100) */}
+          <Card className="mx-auto max-w-sm">
+            <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content flex items-center justify-between">
+              <span>Rating Product A</span>
+              <span>${valor2}</span>
+            </p>
+            <CategoryBar
+              values={[40, 30, 20, 10]}
+              colors={["emerald", "yellow", "orange", "rose"]}
+              markerValue={valor2}
+              className="mt-3"
+            />
+          </Card>
         </div>
       </div>
+
+      {/* Componente de Calendario */}
+      <p className="text-center font-mono text-sm text-slate-600">
+        Seleccione una fecha
+      </p>
+      <DatePicker />
+
       <Card className="mt-8">
         <Title>Domestic Daily</Title>
         <LineChart
           className="mt-6"
           data={chartData}
-          index="year"
-          categories={['Barbie', 'Oppenheimer']}
-          colors={['pink', 'green']}
-          yAxisWidth={120}
+          index="date"
+          categories={["Barbie", "Oppenheimer"]}
+          colors={["pink", "green"]}
+          yAxisWidth={100}
           valueFormatter={addCommasToNumber}
         />
       </Card>
     </div>
   );
-}
+};
 
 export default Dashboard;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -54,8 +54,9 @@ module.exports = {
           background: {
             muted: "#131A2B", // custom
             subtle: "#1f2937", // gray-800
-            DEFAULT: "#111827", // gray-900
+            DEFAULT: "#003049", // gray-900
             emphasis: "#d1d5db", // gray-300
+            emphasis2: "#60a5fa", // blue-400
           },
           border: {
             DEFAULT: "#1f2937", // gray-800

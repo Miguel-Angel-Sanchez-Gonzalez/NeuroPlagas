b982994e3982bbe958b778c817056b83412cb569
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2024-05-30 14:02:33 -0600

    Se agrega nuevo diseño al Dashboard

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -16,7 +16,7 @@ import { useEffect, useState } from "react";
 import "./Dashboard.css"; // Importa el archivo CSS
 import dataBarbie from "../../movie-barbie.json";
 import dataOppenheimer from "../../movie-oppenheimer.json";
-import ComboBoxGreenHouse from './ComboBoxGreenHouse/ComboBoxGreenHouse'; 
+import ComboBoxGreenHouse from "./ComboBoxGreenHouse/ComboBoxGreenHouse";
 
 const Dashboard = () => {
   const chartData = dataBarbie.domestic_daily.map(({ revenue, date }) => {
@@ -42,130 +42,146 @@ const Dashboard = () => {
     { name: "Singapore", sales: 190 },
   ];
 
-  const valueFormatter = (number) => `$ ${Intl.NumberFormat("us").format(number).toString()}`;
+  const valueFormatter = (number) =>
+    `$ ${Intl.NumberFormat("us").format(number).toString()}`;
 
   const datahero = [
-    { name: '/home', value: 456 },
-    { name: '/imprint', value: 351 },
-    { name: '/cancellation', value: 51 },
+    { name: "/home", value: 456 },
+    { name: "/imprint", value: 351 },
+    { name: "/cancellation", value: 51 },
   ];
 
   const chartdata = [
-    { name: 'opens', value: 200 },
-    { name: 'visitors', value: 351 },
-    {
-      name: "added to cart",
-      value: 191,
-    },
-    { name: 'orders', value: 10 },
+    { name: "opens", value: 200 },
+    { name: "visitors", value: 351 },
+    { name: "added to cart", value: 191 },
+    { name: "orders", value: 10 },
   ];
 
   const [selectedGreenhouseId, setSelectedGreenhouseId] = useState(null);
-  const [miValor, setMiValor] = useState("0");
+  const [selectedGreenhouseName, setSelectedGreenhouseName] = useState("");
+  const [numPlagas, setNumPlagas] = useState("0");
+  const [numEnfermedades, setNumEnfermedades] = useState("0");
 
-
-  const handleSelectionChange = (selectedGreenhouseName, selectedGreenhouseId) => {
-    // Aquí puedes hacer lo que necesites con el nombre y el id_invernadero seleccionados
-    console.log("Nombre del invernadero seleccionado:", selectedGreenhouseName);
-    console.log("ID del invernadero seleccionado:", selectedGreenhouseId);
-  
-    // Actualiza el estado con el id_invernadero seleccionado
+  const handleSelectionChange = (
+    selectedGreenhouseName,
+    selectedGreenhouseId
+  ) => {
     setSelectedGreenhouseId(selectedGreenhouseId);
-    setMiValor(selectedGreenhouseId);
-  };
-  
+    setSelectedGreenhouseName(selectedGreenhouseName);
 
+    fetch(
+      `http://localhost:3000/dashboard/getTotalPlaguesByIdGreenhouse/${selectedGreenhouseId}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setNumPlagas(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+
+    fetch(
+      `http://localhost:3000/dashboard/getTotalDiseasesByIdGreenhouse/${selectedGreenhouseId}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setNumEnfermedades(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+    console.log("Nombre actual actualizado: " , selectedGreenhouseName);  
+  };
 
   return (
     <div className="main-div bg-gray-100">
-      <Card className="mt-8">
-        <Title className="text-medium">Seleccione un invernadero</Title>
-        <ComboBoxGreenHouse
-          onChange={handleSelectionChange}
-        />
-      </Card>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
-        <Section>
-          <CardMetric
-            title="Total Plagas"
-            value={miValor}
-          />
-          <CardDonut
-            title="Plagas Distribution"
-            data={sales}
-            valueFormatter={valueFormatter}
-          />
-          <CardCategoryBar
-            title="Product A Rating"
-            value="42"
-            values={[40, 30, 20, 10]}
-          />
-        </Section>
-
-        <Section>
-          <CardMetric
-            title="Total Enfermedades"
-            value={addCommasToNumber(dataOppenheimer.global_revenue)}
-          />
-          <CardDonut
-            title="Enfermedades Distribution"
-            data={sales}
-            valueFormatter={valueFormatter}
-          />
-
-          <CardCategoryBar
-            title="Product A Rating"
-            value="42"
-            values={[40, 30, 20, 10]}
-          />
-        </Section>
-
-        <Section>
-          <CardMetric title="Total Sanos" value="3,000" />
-          <FunnelChart
-            className="h-60"
-            data={chartdata}
-            onValueChange={(v) => console.log(v)}
-          />
-
-          <BarList
-            data={datahero}
-            sortOrder="ascending"
-            className="mx-auto max-w-sm"
-          />
-        </Section>
+      <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 mb-8">
+        <Text className="text-lg-medium md:mt-4">Seleccione invernadero</Text>
+        <ComboBoxGreenHouse onChange={handleSelectionChange} />
+        <Text className="text-lg-medium md:mt-4">Seleccione una fecha</Text>
+        <DatePicker className="md:mt-4" />
       </div>
 
-      <div className="text-center mb-8">
-        <Text className="text-lg-medium">Seleccione una fecha</Text>
-        <DatePicker />
-      </div>
+      {selectedGreenhouseId ? (
+        <>
+          {/* Fila 1 */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-5">
+            <Section>
+              <CardMetric title="Total de Plagas" value={numPlagas} />
+            </Section>
 
-      <Card>
-        <Title className="text-medium">Domestic Daily Revenue Comparison</Title>
-        <LineChart
-          className="mt-6"
-          data={chartData}
-          index="date"
-          categories={["Barbie", "Oppenheimer"]}
-          colors={["purple", "fuchsia"]}
-          yAxisWidth={100}
-          valueFormatter={addCommasToNumber}
-        />
-      </Card>
+            <Section>
+              <CardMetric title="Total de Enfermedades" value={numEnfermedades} />
+            </Section>
+
+            <Section>
+              <Card className="mx-auto max-w-sm">
+                <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content flex items-center justify-between">
+                  <span>Estado del {selectedGreenhouseName}</span>
+                  <span>{numPlagas}%</span>
+                </p>
+                <CategoryBar
+                  values={[40, 30, 20, 10]}
+                  colors={["emerald", "yellow", "orange", "rose"]}
+                  markerValue={numPlagas}
+                  className="mt-3"
+                />
+              </Card>
+            </Section>
+          </div>
+          {/* Fila 2 */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 mb-8">
+            <Section>
+              <CardDonut
+                title={`Distribución de plagas en el invernadero ${selectedGreenhouseName}`}
+                data={sales}
+                valueFormatter={valueFormatter}
+              />
+            </Section>
+
+            <Section>
+              <CardDonut
+                title={`Distribución de enfermedades en el invernadero ${selectedGreenhouseName}`}
+                data={sales}
+                valueFormatter={valueFormatter}
+              />
+            </Section>
+          </div>
+
+          <Card>
+            <Title className="text-medium">
+              Plagas y enfermedades a través del tiempo
+            </Title>
+            <LineChart
+              className="mt-6"
+              data={chartData}
+              index="date"
+              categories={["Barbie", "Oppenheimer"]}
+              colors={["purple", "fuchsia"]}
+              yAxisWidth={100}
+              valueFormatter={addCommasToNumber}
+            />
+          </Card>
+        </>
+      ) : (
+        <Text className="text-lg-medium text-center mt-8">
+          Por favor, seleccione un invernadero para ver los datos.
+        </Text>
+      )}
     </div>
   );
 };
 
-// COMPONENTES DE TREMOR
+const Section = ({ children }) => (
+  <div className="col-span-1 md:col-span-2 lg:col-span-1">{children}</div>
+);
 
-const Section = ({ title, children }) => (
-  <div>
-    <Title className="section-title">{title}</Title>
-    <div className="space-y-6">
-      {children}
-    </div>
-  </div>
+const CardMetric = ({ title, value }) => (
+  <Card className="max-w-full">
+    <Text className="text-medium">{title}</Text>
+    <Metric>{value}</Metric>
+  </Card>
 );
 
 const CardDonut = ({ title, data, valueFormatter }) => (
@@ -178,36 +194,14 @@ const CardDonut = ({ title, data, valueFormatter }) => (
         index="name"
         valueFormatter={valueFormatter}
         colors={["blue", "cyan", "indigo", "violet", "fuchsia"]}
-        className="w-40"
+        className="w-full md:w-2/5"
       />
       <Legend
-        categories={data.map(item => item.name)}
+        categories={data.map((item) => item.name)}
         colors={["blue", "cyan", "indigo", "violet", "fuchsia"]}
         className="max-w-xs"
       />
     </div>
-  </Card>
-);
-
-const CardMetric = ({ title, value }) => (
-  <Card className="max-w-full">
-    <Text className="text-medium">{title}</Text>
-    <Metric>{value}</Metric>
-  </Card>
-);
-
-const CardCategoryBar = ({ title, value, values }) => (
-  <Card className="max-w-full">
-    <div className="flex items-center justify-between mb-4">
-      <Text className="text-medium">{title}</Text>
-      <Metric>{value}</Metric>
-    </div>
-    <CategoryBar
-      values={values}
-      colors={["emerald", "yellow", "orange", "rose"]}
-      markerValue={value}
-      className="mt-3"
-    />
   </Card>
 );
 

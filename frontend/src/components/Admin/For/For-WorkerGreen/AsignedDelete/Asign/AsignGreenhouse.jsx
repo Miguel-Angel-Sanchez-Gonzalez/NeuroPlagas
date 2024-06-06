d34fe793fd90a5d3b853df404f84bbca448f6a6b
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2024-06-06 17:43:19 -0600

    Se agrega la lista de invernaderos para la asignacion a trabajadores

--- frontend/src/components/Admin/For/For-WorkerGreen/AsignedDelete/Asign/AsignGreenhouse.jsx
+++ frontend/src/components/Admin/For/For-WorkerGreen/AsignedDelete/Asign/AsignGreenhouse.jsx
@@ -1,34 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './AsignGreenhouse.css';
 import AddNotification from '../../../../../LoginNotifications/AddNotification';
+import ComboBoxGreenHouse from '../../../../../Dashboard/ComboBoxGreenHouse/ComboBoxGreenHouse';
 
 const AsignGreenhouse = ({ onCancelClick, idWorker, idFarmer }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [loadingMessage, setLoadingMessage] = useState('');
   const [idGreenhouse, setIdGreenhouse] = useState('');
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [isActive, setIsActive] = useState(false);
-  const [options, setOptions] = useState([]);
-  const [selected, setSelected] = useState(null);
 
-  // useEffect(() => {
-  //   getGreenByIdFarmer();
-  // }, [idFarmer]);
-
-  // //CORREGIR 
-  // const getGreenByIdFarmer = async () => {
-  //   const response = await fetch(`http://localhost:3000/farmer/getNameFarmers`)
-  //   const data = await response.json()
-  //   setOptions(data.map(greenhouse => {
-  //       return {label: greenhouse.nombre, 
-  //         value:greenhouse.id_invernadero}
-  //   }));  
-  // };
-
-  const handleOptionClick = (option) => {
-    //setIdGreenhouse(option.value);
-    setSelected(option.value === undefined ? "" : option);
-    setIsActive(false);
+  const handleSelectionChange = (selectedGreenhouseName, selectedGreenhouseId) => {
+    setIdGreenhouse(selectedGreenhouseId);  // Asegúrate de que el idGreenhouse se actualiza
   };
 
   const handleSubmit = async (e) => {
@@ -36,6 +18,7 @@ const AsignGreenhouse = ({ onCancelClick, idWorker, idFarmer }) => {
     setIsFormSubmitted(true);
 
     if (!idGreenhouse) {
+      setLoadingMessage('Por favor, seleccione un invernadero antes de guardar.');
       return;
     }
 
@@ -77,35 +60,7 @@ const AsignGreenhouse = ({ onCancelClick, idWorker, idFarmer }) => {
           <h4 className="h4register-bed">Asignar invernadero al agricultor</h4>
           <h5 className="h5register-bed">*Campo requerido</h5>
           <label className="label-dato-bed">Por favor, seleccione un invernadero</label>
-          <div className="form-sec-bed-register">
-            <div className="column-register-bed">
-              <label className={`label-bed-r ${isFormSubmitted && !idGreenhouse ? 'red-label' : ''}`}>
-                Invernaderos*
-              </label>
-              <div className="dropdown">
-                <div
-                  className={`dropdown-btn ${isFormSubmitted && !selected ? 'red-input' : ''}`}
-                  style={selected ? { backgroundColor: '#EFF6FF' } : null}
-                  onClick={() => setIsActive(!isActive)}
-                >
-                  {selected || "Seleccionar invernadero..."}
-                </div>
-                {isActive && (
-                  <div className="dropdown-content">
-                    {options.map((option) => (
-                      <div
-                        key={option.value}
-                        onClick={() => handleOptionClick(option)}
-                        className="dropdown-option"
-                      >
-                        {option.label}
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
+          <ComboBoxGreenHouse onChange={handleSelectionChange} />
           <div className="button-container-bed">
             <button className="button-bed" type="submit" onClick={handleSubmit}>Guardar</button>
             <button className="button-bed" onClick={onCancelClick}>Cancelar</button>

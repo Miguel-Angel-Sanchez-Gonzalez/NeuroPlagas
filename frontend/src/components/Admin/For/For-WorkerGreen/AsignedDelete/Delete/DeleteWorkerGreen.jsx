ea4bd136be1bba85b36e23d31271f683820e7b06
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2024-06-06 21:44:34 -0600

    Se agrega la nueva notificacion al desasignar un invernadero al trabajador

--- frontend/src/components/Admin/For/For-WorkerGreen/AsignedDelete/Delete/DeleteWorkerGreen.jsx
+++ frontend/src/components/Admin/For/For-WorkerGreen/AsignedDelete/Delete/DeleteWorkerGreen.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './DeleteWorkerGreen.css';
+import AddNotification from '../../../../../LoginNotifications/AddNotification';
 
 const DeleteWorkerGreen = ({ onCancelClick, idWorkerGreenhouse }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingMessage, setLoadingMessage] = useState('');
+
 
   const onConfirmClick = () => {
+    
     fetch(`http://localhost:3000/worker/deleteAsignGreenhouse/${idWorkerGreenhouse}`, {
       method: 'DELETE',
       headers: {
@@ -12,16 +17,21 @@ const DeleteWorkerGreen = ({ onCancelClick, idWorkerGreenhouse }) => {
     })
       .then(response => {
         if (response.ok) {
-          alert("El invernadero se ha desasignado con éxito");
-          onCancelClick();
-          window.location.reload();
+          setIsLoading(true);
+          setLoadingMessage('El invernadero se ha desasignado con éxito.');
+          setTimeout(() => {
+            setLoadingMessage('');
+            window.location.reload();
+          }, 2000);
         } else {
           alert("Error al desasignar el invernadero");
+          setIsLoading(false);
         }
       })
       .catch(error => {
         console.error('Error al desasignar el invernadero:', error);
         alert("Error al desasignar el invernadero");
+        setIsLoading(false);
       });
   }
   
@@ -35,6 +45,9 @@ const DeleteWorkerGreen = ({ onCancelClick, idWorkerGreenhouse }) => {
         <button className='button-delete-farmer' type="submit" onClick={onConfirmClick}>Desasignar</button>
         <button className='btn-delete-farmer-cancel' onClick={onCancelClick}>Cancelar</button>
       </div>
+      {loadingMessage && (
+            <AddNotification message={loadingMessage} onClose={() => setLoadingMessage('')} className="farmer-notification" />
+          )}
     </div>
   );
 };
